76e49f78fb25eeb49dff61df69eacb93f6299e57
Felipe Yui <felipe_m87@yahoo.com.br>
2025-05-06 00:01:38 +0930

    add error handler

--- apps/api/src/http/routes/auth/get-profile.ts
+++ apps/api/src/http/routes/auth/get-profile.ts
@@ -4,6 +4,8 @@ import z from 'zod'
 
 import { prisma } from '@/lib/prisma'
 
+import { BadRequestError } from '../_errors/bad-request-error'
+
 export async function getProfile(app: FastifyInstance) {
   app.withTypeProvider<ZodTypeProvider>().get(
     '/profile',
@@ -20,9 +22,6 @@ export async function getProfile(app: FastifyInstance) {
               email: z.string(),
               avatarUrl: z.string().nullable(),
             }),
-          }),
-          401: z.object({
-            message: z.string(),
           }),
         },
       },
@@ -43,7 +42,7 @@ export async function getProfile(app: FastifyInstance) {
       })
 
       if (!user) {
-        throw new Error('User not found')
+        throw new BadRequestError('User not found')
       }
 
       return reply.status(200).send({ user })
