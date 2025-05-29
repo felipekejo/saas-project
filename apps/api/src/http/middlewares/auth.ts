42a93d1b0356d2a268599016bf0a4948ae1e4701
Felipe Yui <felipe_m87@yahoo.com.br>
2025-05-29 17:12:42 +0930

    get membership

--- apps/api/src/http/middlewares/auth.ts
+++ apps/api/src/http/middlewares/auth.ts
@@ -1,6 +1,8 @@
 import type { FastifyInstance } from 'fastify'
 import fastifyPlugin from 'fastify-plugin'
 
+import { prisma } from '@/lib/prisma'
+
 import { UnauthorisedError } from '../routes/_errors/unauthorised-error'
 
 export const auth = fastifyPlugin((app: FastifyInstance) => {
@@ -11,6 +13,31 @@ export const auth = fastifyPlugin((app: FastifyInstance) => {
         return sub
       } catch (error) {
         throw new UnauthorisedError('Invalid Auth Token')
+      }
+    }
+
+    request.getUserMemberShip = async (slug: string) => {
+      const userId = await request.getCurrentUserId()
+      const member = await prisma.member.findFirst({
+        where: {
+          userId,
+          organization: {
+            slug,
+          },
+        },
+        include: {
+          organization: true,
+        },
+      })
+      if (!member) {
+        throw new UnauthorisedError('You are not a member of this organization')
+      }
+
+      const { organization, ...membership } = member
+
+      return {
+        organization,
+        membership,
       }
     }
   })
