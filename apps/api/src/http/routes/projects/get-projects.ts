e28a377679f85123ff636d4eb5bcfa656a126e15
Felipe Yui <felipe_m87@yahoo.com.br>
2025-07-01 23:33:50 +0930

    get projects

---
+++ apps/api/src/http/routes/projects/get-projects.ts
@@ -0,0 +1,91 @@
+import type { FastifyInstance } from 'fastify'
+import type { ZodTypeProvider } from 'fastify-type-provider-zod'
+import z from 'zod'
+
+import { auth } from '@/http/middlewares/auth'
+import { prisma } from '@/lib/prisma'
+import { getUserPermissions } from '@/utils/get-user-permissions'
+
+import { UnauthorisedError } from '../_errors/unauthorised-error'
+
+export async function getProjects(app: FastifyInstance) {
+  app
+    .withTypeProvider<ZodTypeProvider>()
+    .register(auth)
+    .get(
+      '/organizations/:slug/projects',
+      {
+        schema: {
+          tags: ['projects'],
+          summary: 'Get all organization projects',
+          security: [{ bearerAuth: [] }],
+          params: z.object({
+            slug: z.string(),
+          }),
+          response: {
+            200: z.object({
+              projects: z.array(
+                z.object({
+                  name: z.string(),
+                  id: z.string().uuid(),
+                  slug: z.string(),
+                  avatarUrl: z.string().nullable(),
+                  ownerId: z.string().uuid(),
+                  organizationId: z.string().uuid(),
+                  description: z.string().nullable(),
+                  createdAt: z.date(),
+                  owner: z.object({
+                    id: z.string().uuid(),
+                    name: z.string().nullable(),
+                    avatarUrl: z.string().nullable(),
+                  }),
+                }),
+              ),
+            }),
+          },
+        },
+      },
+      async (request, reply) => {
+        const { slug } = request.params
+        const userId = await request.getCurrentUserId()
+        const { membership, organization } =
+          await request.getUserMemberShip(slug)
+
+        const { cannot } = getUserPermissions(userId, membership.role)
+
+        if (cannot('get', 'Project')) {
+          throw new UnauthorisedError(
+            'You are not authorized to see projects in this organization',
+          )
+        }
+
+        const projects = await prisma.project.findMany({
+          select: {
+            id: true,
+            name: true,
+            description: true,
+            slug: true,
+            ownerId: true,
+            avatarUrl: true,
+            organizationId: true,
+            createdAt: true,
+            owner: {
+              select: {
+                id: true,
+                name: true,
+                avatarUrl: true,
+              },
+            },
+          },
+          where: {
+            organizationId: organization.id,
+          },
+          orderBy: {
+            createdAt: 'desc',
+          },
+        })
+
+        return reply.status(200).send({ projects })
+      },
+    )
+}
