17c1ae84644e100e343d4fadaba19dab1f24fa43
Felipe Yui <felipe_m87@yahoo.com.br>
2025-07-03 22:33:36 +0930

    remove member

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -19,6 +19,7 @@ import { getProfile } from './routes/auth/get-profile'
 import { requestPasswordRecover } from './routes/auth/request-password-recover'
 import { resetPassword } from './routes/auth/reset-password'
 import { getMembers } from './routes/members/get-members'
+import { removeMember } from './routes/members/remove-member'
 import { updateMember } from './routes/members/update-member'
 import { createOrganization } from './routes/orgs/create-organizatrion'
 import { getMembership } from './routes/orgs/get-membership'
@@ -78,6 +79,7 @@ app.register(requestPasswordRecover)
 app.register(resetPassword)
 app.register(createOrganization)
 app.register(getMembership)
+
 app.register(getOrganization)
 app.register(getOrganizations)
 app.register(updateOrganization)
@@ -88,8 +90,10 @@ app.register(deleteProject)
 app.register(getProject)
 app.register(getProjects)
 app.register(updateProject)
+
 app.register(getMembers)
 app.register(updateMember)
+app.register(removeMember)
 
 app.listen({ port: env.SERVER_PORT }).then(() => {
   console.log('HTTP server is running on port 3333')
