9c8786a3f110e2ec4688db9534f15214e9672da7
Felipe <felipe@cortexinteractive.com.au>
2025-07-29 16:15:09 +0930

    setup client http (ky)

--- apps/web/src/app/auth/sign-in/actions.tsx
+++ apps/web/src/app/auth/sign-in/actions.tsx
@@ -1,5 +1,15 @@
 'use server'
 
+import { api } from "@/http/api-client"
+import { signInWithPassword } from "@/http/sign-in-with-password"
+
+
+
 export async function signInWithEmailAndPassword(data:FormData){
-  console.log(Object.fromEntries(data))
+  const {email, password} = Object.fromEntries(data)
+
+  const result = await signInWithPassword({email:String(email), password:String(password)})
+
+  console.log(result)
+
 }
